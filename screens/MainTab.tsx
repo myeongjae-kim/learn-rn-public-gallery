2a3be7d18bc7cea001d378a7be00a4b04e43b84b
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-04-24 00:27:08 +0900

    feat: CameraButton, Modal, ActionSheetIOS

--- screens/MainTab.tsx
+++ screens/MainTab.tsx
@@ -6,6 +6,7 @@ import {
 import HomeStack from './HomeStack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MyProfileStack from './MyProfileStack';
+import CameraButton from '../components/CameraButton';
 
 type MainTabParamList = {
   HomeStack: undefined;
@@ -18,32 +19,46 @@ const Tab = createBottomTabNavigator<MainTabParamList>();
 
 const MainTab = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#6200ee',
-      }}>
-      <Tab.Screen
-        name={'HomeStack'}
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon name={'home'} size={24} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={'MyProfileStack'}
-        component={MyProfileStack}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon name={'person'} size={24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200ee',
+          }}>
+          <Tab.Screen
+            name={'HomeStack'}
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name={'home'} size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={'MyProfileStack'}
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name={'person'} size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+      <CameraButton />
+    </>
   );
 };
+
+import {StyleSheet, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    zIndex: 0,
+  },
+});
 
 export default MainTab;
